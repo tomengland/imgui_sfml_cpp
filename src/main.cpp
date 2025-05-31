f9a7fc90c5ec38b27eb89211087e14ddcf5b6287
THOMAS ENGLAND <tom@england.dev>
2025-05-30 17:34:44 -1000

    update

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main()
         std::cerr << "No shapes found in the configuration file." << std::endl;
         return 1;
     }
-    sf::RenderWindow window(sf::VideoMode({config.wWidth, config.wHeight}), "Clair Expedition: 34");
+    sf::RenderWindow window(sf::VideoMode({config.wWidth, config.wHeight}), "Clair Expedition: 34 .. heh");
     window.setFramerateLimit(60);
 
     // initialize IMGUI
